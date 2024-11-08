888a16709c559f1bbf8211eb00dc16d1c16f82b3
Bryan <bryandang10@gmail.com>
2024-11-08 09:36:17 -0800

    Show and tell video 1

--- src/components/Videos.tsx
+++ src/components/Videos.tsx
@@ -33,7 +33,7 @@ const Videos: React.FC = () => {
         <Grid item xs={12} md={8}>
           <Box 
             component="iframe"
-            src="https://www.youtube.com/embed/yigWHRhEh5c"
+            src="https://youtu.be/2Dzo_elfJW8"
             title="Show and Tell 1"
             width="100%"
             height="315px"
